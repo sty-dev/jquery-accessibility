93a4a0330ee3e50be6b94e48198e5851b496a4c2
arai takayuki <arai_takayuki@stylement.co.jp>
2017-11-06 15:26:48 +0900

    demo update

---
+++ demo/js/init.js
@@ -0,0 +1,19 @@
+jQuery(function($){
+
+  $('.jq_a11y--root').a11y();
+
+  $('.jq_a11y--expand').parent().on({
+    mouseenter: function () {
+      $(this).children('.jq_a11y--expand').addClass('active');
+      $(this).children('.jq_a11y--expand').siblings('.dropdown-list').stop();
+      $(this).children('.jq_a11y--expand').siblings('.dropdown-list').fadeIn(300);
+      $(this).children('.jq_a11y--expand').a11y('expand');
+    },
+    mouseleave: function () {
+      $(this).children('.jq_a11y--expand').removeClass('active');
+      $(this).children('.jq_a11y--expand').siblings('.dropdown-list').stop();
+      $(this).children('.jq_a11y--expand').siblings('.dropdown-list').fadeOut(300);
+      $(this).children('.jq_a11y--expand').a11y('expand');
+    }
+  });
+});
